1a1d8f4f10e1a9196b744688ff38dabcec815cc4
chenzhenan <chenzhenan@quseit.com>
2016-11-16 18:34:17 +0800

    add mac

--- An/run_hear.py
+++ An/run_hear.py
@@ -1,15 +1,17 @@
 # 在指定文件夹运行程序
 import sublime, sublime_plugin, os
-from utils.path import run_at
 from An import an
+from subl import load_settings
+from utils.path import run_at
 
 class RunHearCommand(sublime_plugin.WindowCommand):
 	def run(self, path = None):
-		self.run_list_data = sublime.load_settings('an_run_hear.sublime-settings').get('run_list')[sublime.platform()]
-		run_list = [[item['title'], item['path']] for item in self.run_list_data]
-		if not path:
-			path = os.path.dirname(self.window.active_view().file_name())
-		self.window.show_quick_panel(run_list, run_at(self.onselect, path), selected_index=an._run_hear_last or -1)
+		self.run_list_data = load_settings(self, sublime.platform())
+		if self.run_list_data:
+			run_list = [[item['title'], item['path']] for item in self.run_list_data]
+			if not path:
+				path = os.path.dirname(self.window.active_view().file_name())
+			self.window.show_quick_panel(run_list, run_at(self.onselect, path), selected_index=an._run_hear_last or -1)
 
 	def onselect(self, index):
 		if index == -1:

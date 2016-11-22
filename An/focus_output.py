6f9396abe55a280b2b997609a498aaf94a499fbc
Chenzhenan <2464851375@qq.com>
2016-11-22 15:05:52 +0800

    focus output

--- An/focus_output.py
+++ An/focus_output.py
@@ -4,10 +4,11 @@ from An import an
 class FocusOutputCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit):
+		an.view = self.view
+		an.tout()
 		if self.view.view_id == an.output.view_id:
 			if an.last_group is not None:
 				self.view.window().focus_group(an.last_group)
 		else:
 			an.last_group = self.view.window().active_group()
-			an.tout()
 			self.view.window().focus_view(an.output)

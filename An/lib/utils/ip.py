92c3f7385e02121a4b832957fa6b5d19e858570b
Chenzhenan <2464851375@qq.com>
2017-04-06 09:25:01 +0800

    make it better

--- An/lib/utils/ip.py
+++ An/lib/utils/ip.py
@@ -1,15 +1,23 @@
 import socket
+
 def get_eth_ip(ethname):
 	import fcntl, struct
-	s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0X8915, struct.pack('256s', ethname[:15].encode('utf-8')))[20:24])
 
+
 def get_linux_local_ip():
-	return get_eth_ip('wlp2s0')
+	for x in ('wlp2s0', 'enp3s0f2'):
+		try:
+			return get_eth_ip(x)
+		except OSError:
+			print('read %s failed' % x)
+
 
 def get_windows_local_ip():
 	import socket
 	return socket.gethostbyname(socket.gethostname())
+
 
 def get_ip():
 	from utils import runtime

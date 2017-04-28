4a8fe42bf2a1149b680366241fbcc0301b27ffe2
Chenzhenan <2464851375@qq.com>
2017-04-28 15:58:40 +0800

    make it better

--- An/scripts/apitest.py
+++ An/scripts/apitest.py
@@ -2,13 +2,15 @@ from utils.thread import newthread
 import request
 
 def wrapper(fn):
-	@newthread
-	def _deco(host, *args, **keyArgs):
-		try:
-			an.echo(fn(an.host + host, *args, **keyArgs))
-		except Exception as e:
-			an.echo(e)
-	return _deco
+    @newthread
+    def _deco(host, *args, **keyArgs):
+        try:
+            result = fn(an.host + host, *args, **keyArgs)
+            result = result.encode().decode('unicode_escape')
+            an.echo(result)
+        except Exception as e:
+            an.echo(e)
+    return _deco
 
 an.api_get = wrapper(request.get)
 an.api_post = wrapper(request.post)

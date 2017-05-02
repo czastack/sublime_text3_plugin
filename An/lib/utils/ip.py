8c10234aa624fa26eb47f42c20c1f9e225e00f50
陈振安 <an@chenzhenandeMBP.lan>
2017-05-02 17:17:51 +0800

    make it better

--- An/lib/utils/ip.py
+++ An/lib/utils/ip.py
@@ -1,27 +1,27 @@
 import socket
 
 def get_eth_ip(ethname):
-	import fcntl, struct
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0X8915, struct.pack('256s', ethname[:15].encode('utf-8')))[20:24])
+    import fcntl, struct
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0X8915, struct.pack('256s', ethname[:15].encode('utf-8')))[20:24])
 
 
 def get_linux_local_ip():
-	for x in ('wlp2s0', 'enp3s0f2'):
-		try:
-			return get_eth_ip(x)
-		except OSError:
-			print('read %s failed' % x)
+    for x in ('wlp2s0', 'enp3s0f2'):
+        try:
+            return get_eth_ip(x)
+        except OSError:
+            print('read %s failed' % x)
 
 
 def get_windows_local_ip():
-	import socket
-	return socket.gethostbyname(socket.gethostname())
+    import socket
+    return socket.gethostbyname(socket.gethostname())
 
 
 def get_ip():
-	from utils import runtime
-	if runtime.is_linux:
-		return get_linux_local_ip()
-	else:
-		return get_windows_local_ip()
+    from utils import runtime
+    if runtime.is_linux:
+        return get_linux_local_ip()
+    elif runtime.is_windows:
+        return get_windows_local_ip()

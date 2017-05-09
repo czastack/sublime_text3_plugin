7ef81b8909ec9407b0f3aa776753f0ded084106a
陈振安 <chenzhenan@yuhuofei.it>
2017-05-09 15:07:53 +0800

    for mac

--- An/run_hear.py
+++ An/run_hear.py
@@ -8,7 +8,7 @@ class RunHearCommand(sublime_plugin.WindowCommand):
 	def run(self, path = None):
 		self.run_list_data = load_settings(self, sublime.platform())
 		if self.run_list_data:
-			run_list = [[item['title'], item['path']] for item in self.run_list_data]
+			run_list = [[item['title'], item['cmd']] for item in self.run_list_data]
 			if not path:
 				path = os.path.dirname(self.window.active_view().file_name())
 			self.window.show_quick_panel(run_list, run_at(self.onselect, path), selected_index=an._run_hear_last or -1)
@@ -17,8 +17,6 @@ class RunHearCommand(sublime_plugin.WindowCommand):
 		if index == -1:
 			return
 		an._run_hear_last = index
-		cmd = self.run_list_data[index]['path']
-		if sublime.platform() == 'windows':
-			cmd = 'start ' + cmd
+		cmd = self.run_list_data[index]['cmd']
 		p = os.popen(cmd)
 		p.close()
